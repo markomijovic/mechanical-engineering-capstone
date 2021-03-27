51e8ef655523063575f566b52dd0ccefebafaced
Marko Mijovic <mijovicmarko11@gmail.com>
2021-03-26 22:26:42 -0600

    first init

---
+++ src/main.py
@@ -0,0 +1,20 @@
+### Author: Marko Mijovic, mijovicmarko11@gmail.com
+### Date: March 26, 2021
+"""
+This program is a part of the 2020/21 mechanical engineering capstone project. The program preforms
+statistical analysis and creates unique visualizations on bearing accelerometer data obtained from
+https://www.kaggle.com/vinayak123tyagi/bearing-dataset?select=1st_test
+Program is for academic and learning purposes only.
+"""
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/

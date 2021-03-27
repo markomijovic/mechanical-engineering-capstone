6f928dbfaaed1522c2aadf91b6796608a2fa0c56
Marko Mijovic <mijovicmarko11@gmail.com>
2021-03-26 23:33:00 -0600

    data read test

---
+++ src/readdatatest.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+import numpy as np
+dir = '../data/test/'
+filenames = [name for name in os.listdir(dir) if os.path.isfile(os.path.join(dir, name))]
+samples_per_row = len(pd.read_csv(os.path.join(dir, filenames[0]), sep="\t", nrows=1).columns)
+
+x_values = np.zeros([len(filenames), 20480, samples_per_row]) # makes a 3D array of #offiles by 20480 (rows per file) by number of columns in file
+filenames = sorted(filenames)
+for i,file in enumerate(filenames):
+    x_values[i:i+1,:,:] =np.fromfile(os.path.join(dir, file), dtype=float, sep=" ").reshape(20480,samples_per_row)
+print(x_values)

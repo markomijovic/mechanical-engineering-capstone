e9645c7592af72ca06c256ecb03d0383f6fbeaae
Marko Mijovic <mijovicmarko11@gmail.com>
2021-04-06 17:23:45 -0600

    chaning to OOP

--- src/main.py
+++ src/main.py
@@ -6,44 +6,39 @@ statistical analysis and creates unique visualizations on bearing accelerometer 
 https://www.kaggle.com/vinayak123tyagi/bearing-dataset?select=1st_test
 Program is for academic and learning purposes only.
 """
-from readdata import *
+from readData import *
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-### @Params None
-### Gets the acceleration data stored in acceleration.db
-### Returns the acceleration as a pandas dataframe
-def get_acceleration():
-    # for some reason does not work without full directory path
-    dat = sqlite3.connect(r"C:\Users\Marko\Desktop\School\CS - Personal\Projects\Personal - Capstone\main\data\processed\acceleration.db")
-    query = dat.execute("SELECT * From Acceleration")
-    cols = [column[0] for column in query.description]
-    df = pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
-    return df
+class Acceleration:
 
-def create_box_plot(df):
-    fig, axes = plt.subplots(3, 2)
-    fig.suptitle('Acceleration Statistical Distribution. Failure on Bearings 3 and 4')
-    sns.boxplot(ax=axes[0, 0], x=df["x1"])
-    sns.boxplot(ax=axes[0, 1], x=df["y1"])
-    sns.boxplot(ax=axes[1, 0], x=df["x3"])
-    sns.boxplot(ax=axes[1, 1], x=df["y3"])
-    sns.boxplot(ax=axes[2, 0], x=df["x4"])
-    sns.boxplot(ax=axes[2, 1], x=df["y4"])
-    plt.show()
+    def __init__(self, path) -> None:
+        self.path = path
 
-def create_heat_plot(df):
-    sns.heatmap(df.corr(), linewidths=.5, annot=True, fmt=".2f")
-    plt.show()
-
-
-def create_line_plot(df):
-    print("inside line plot")
-    df_reshaped = reshape_data(df)
-    print("reshaped df. plotting ...")
-    sns.lineplot(data=df_reshaped, x="Time", y="Acceleration", hue="Label", alpha=0.5)
-    print("finished plotting. opening the plot...")
-    plt.show()
+    def get_acceleration_from_db(self) -> pd.DataFrame:
+        dat = sqlite3.connect(self.path_db)
+        query = dat.execute("SELECT * From Acceleration")
+        cols = [column[0] for column in query.description]
+        return pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
+        
+    def create_box_plot(self, df):
+        fig, axes = plt.subplots(2, 2)
+        fig.suptitle('Acceleration Statistical Distribution. Failure on 1')
+        sns.boxplot(ax=axes[0, 0], x=df["a1"])
+        sns.boxplot(ax=axes[0, 1], x=df["a2"])
+        sns.boxplot(ax=axes[1, 0], x=df["a3"])
+        sns.boxplot(ax=axes[1, 1], x=df["a4"])
+        plt.show()
+    
+    def create_heat_plot(self, df):
+        sns.heatmap(df.corr(), linewidths=.5, annot=True, fmt=".2f")
+        plt.show()
+'''
+    def create_line_plot(self, df):
+        df_reshaped = reshape_data(df)
+        sns.lineplot(data=df_reshaped, x="Time", y="Acceleration", hue="Label", alpha=0.5)
+        print("finished plotting. opening the plot...")
+        plt.show()
 
 # Helper function for create_line_plot
 def reshape_data(df):
@@ -61,11 +56,10 @@ def reshape_data(df):
         'Label' : label
     })
     return df_new
+'''
 
 if __name__ == "__main__":
-    df = get_acceleration()
-    with pd.option_context('display.max_columns', None):
-        print(df.describe())
-    #create_line_plot(df)
-    create_box_plot(df)
-    create_heat_plot(df)
+    db_path = 'data/processed/acceleration2.db'
+    p_absolute = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', db_path))
+    accel = Acceleration(p_absolute)
+    print(accel.path)
